7f3c0e673f061f45b979f124115d7fc02c002eb6
joakimwidell <joakimwidell@hotmail.com>
2021-04-29 08:44:40 +0200

    Created models

---
+++ Source/SpacePark2/Models/Parking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpacePark2.Models
+{
+    public class Parking
+    {
+        public ParkingHouse ParkingHouseId { get; set; }
+        public SpaceTraveller SpaceTravellerId { get; set; }
+        public StarShip StarShipId  { get; set; }
+        public DateTime ArrivalTime { get; set; } = DateTime.Now;
+        public  DateTime? DepartureTime { get; set; }
+        public int Cost { get; set; } = 0;
+
+    }
+}
---
+++ Source/SpacePark2/Models/StarShip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpacePark2.Models
+{
+    public class StarShip
+    {
+        public int Id { get; set; }
+        public string StarShipModel { get; set; }
+        public double ShipLength { get; set; }
+
+    }
+}
