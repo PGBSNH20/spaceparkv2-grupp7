eb3c6a89e5ba76b9d100f5fa0742594621afd958
joakimwidell <joakimwidell@hotmail.com>
2021-04-29 08:59:56 +0200

    add context

---
+++ Source/SpacePark2/SpaceParkContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SpacePark2.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpacePark2
+{
+    public class SpaceParkContext : DbContext
+    {
+        public DbSet<Parking> Parking { get; set;}
+        public DbSet<ParkingHouse> ParkingHouse { get; set; }
+        public DbSet<SpaceTraveller> SpaceTraveller { get; set; }
+        public DbSet<StarShip> StarShip { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            var config = builder.Build();
+            var defaultConnectionString = config.GetConnectionString("Default");
+            optionsBuilder.UseSqlServer(defaultConnectionString);
+
+        }
+    }
+}
