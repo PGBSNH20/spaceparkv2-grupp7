49d883f38adf305ab20007ebb21f01699b244a23
Baverstrand <70134415+Baverstrand@users.noreply.github.com>
2021-05-03 15:09:12 +0200

    adapted swapi to our methods

--- Service/SwApi.cs
+++ Service/SwApi.cs
@@ -17,7 +17,6 @@ namespace Service
         private string NamePath { get; set; }
         public string ShipName { get; set; }
         public string ShipPath { get; set; }
-       // public double ShipLength { get; set; }
 
         readonly HttpClient Client = new();
 
@@ -55,54 +54,33 @@ namespace Service
         {
             NamePath = $"https://swapi.dev/api/people/?search={name}";
             var search = await GetStarWarsObject<SearchResultTraveller>(NamePath);
-         
-            try
-            {
-                if (search.results[0].Name.ToLower() == name.ToLower())
-                {
-                    return search.results[0];
-                }
-                else
-                {
-                    
-                    Console.WriteLine("You have entered an invalid name, please enter your full name.");
-                }
-            }
-            catch (Exception)
-            {
-                // TODO Hantera här!!
-                Console.WriteLine("You are not famous and can't access this spacepark.");
-                await Task.Delay(2000);
-            };
+
+            if (search.results[0].Name.ToLower() == name.ToLower())
+                return search.results[0];
 
             return null;
         }
 
-        //public async Task<string> ChooseStarShip(SpaceTraveller person)
-        //{
-        //    var menu = new Menu();
-        //    List<string> starShips = new();
-        //    if (!person.StarShips.Any())
-        //    {
-        //        Console.WriteLine("You don't have a spaceship to park. Next, please!");
-        //        await Task.Delay(2000);
-        //        return null;
-        //    }
-
-        //    foreach (var starShip in person.StarShips)
-        //    {
-        //        var search = await GetStarWarsObject<Starship>(starShip);
-        //        starShips.Add(search.Name);
-        //    }
-
-        //    int starShipIndex = menu.ShowMenu("Choose Starship to park: ", starShips);
-
-        //    return starShips[starShipIndex];
-        //}
-
-        public async Task<double> GetShipLength(string path)
+        public async Task<List<string>> ChooseStarShip(SpaceTraveller person)
         {
-            ShipPath = $"https://swapi.dev/api/starships/?search={path}";
+            List<string> starShips = new();
+            if (!person.StarShips.Any())
+            {
+                return null;
+            }
+
+            foreach (var starShip in person.StarShips)
+            {
+                var search = await GetStarWarsObject<Starship>(starShip);
+                starShips.Add(search.Name);
+            }
+
+            return starShips;
+        }
+
+        public async Task<double> GetShipLength(string shipName)
+        {
+            ShipPath = $"https://swapi.dev/api/starships/?search={shipName}";
 
             var starShip = await GetStarWarsObject<SearchResultStarShip>(ShipPath);
             
